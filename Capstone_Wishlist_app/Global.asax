2342c73578ba33479015621b0fb83cbcb40e0545
mesbrook <markesbrook@hotmail.com>
2015-09-14 05:03:43 -0400

    initial setup

---
+++ Capstone_Wishlist_app/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Capstone_Wishlist_app.MvcApplication" Language="C#" %>
---
+++ Capstone_Wishlist_app/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Capstone_Wishlist_app.Startup))]
+namespace Capstone_Wishlist_app
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
