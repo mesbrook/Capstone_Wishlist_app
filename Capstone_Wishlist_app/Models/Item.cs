77b7f35217fcbdd7caa92997ffa35d194176d821
Mark Esbrook <markesbrook@hotmail.com>
2015-10-04 19:43:34 -0400

    Added Items Database and some CSS

---
+++ Capstone_Wishlist_app/Models/Item.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Capstone_Wishlist_app.Models
+{
+    public class Item
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required]
+        public string ASIN { get; set; }
+
+        public string Title { get; set; }
+
+        public string Gender { get; set; }
+
+        public int AgeGroup { get; set; }//set in months
+
+        public int Amount { get; set; } 
+
+        public string FormattedPrice { get; set; }
+
+        [DataType(DataType.ImageUrl)]
+        public string ImageUrlSmall { get; set; }
+
+        [DataType(DataType.ImageUrl)]
+        public string ImageUrlMed { get; set; }
+
+        [DataType(DataType.Url)]
+        public string Url { get; set; }
+
+        public string Features { get; set; }
+    }
+}
