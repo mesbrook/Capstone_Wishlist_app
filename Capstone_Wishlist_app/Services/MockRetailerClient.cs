52de9e84a5c2bc64cdefc8d730eb692a370d0759
eoneillksu <eoneillspsu@gmail.com>
2015-10-13 13:34:05 -0400

    ETO-10-11-15: Began implementing a client for the retail service. Added a mock for the client that reads from a list of items in the project's data folder.

---
+++ Capstone_Wishlist_app/Services/MockRetailerClient.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using RetailService;
+using Newtonsoft.Json;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Capstone_Wishlist_app.Services {
+    public class MockRetailerClient : IRetailerClient {
+        private string _itemFileName;
+
+        public MockRetailerClient(string itemFileName) {
+            _itemFileName = itemFileName;
+        }
+
+        public async Task<Item[]> FindItemsAsync(ItemCategory category, string keywords) {
+            using (var reader = new StreamReader(_itemFileName)) {
+                string serializedItems = reader.ReadToEnd();
+                return JsonConvert.DeserializeObject<Item[]>(serializedItems);
+            }
+        }
+    }
+}
