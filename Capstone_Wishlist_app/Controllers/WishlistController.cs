e83f2db710eacd3de39253fe1052831b87c89f4a
Will Wilson <bladesofwill@users.noreply.github.com>
2015-11-04 21:23:48 -0500

    Update
    I hope this works

---
+++ Capstone_Wishlist_app/Controllers/WishlistController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Capstone_Wishlist_app.Controllers
+{
+    public class WishlistController : Controller
+    {
+        // GET: Wishlist
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
