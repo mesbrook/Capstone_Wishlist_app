08afbffff70a07e08d93d9c3526a49fe582e4d25
eoneillksu <eoneillspsu@gmail.com>
2015-11-24 12:15:22 -0500

    ETO-STORY-20: Added donor controller with view cart and add to cart methods.

---
+++ Capstone_Wishlist_app/Controllers/DonorController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using System.Configuration;
+using System.Threading.Tasks;
+using Capstone_Wishlist_app.Models;
+using Capstone_Wishlist_app.DAL;
+
+namespace Capstone_Wishlist_app.Controllers
+{
+    public class DonorController : Controller
+    {
+        private WishlistContext _db;
+
+        public DonorController() : base() {
+            _db = new WishlistContext();
+        }
+
+        // GET: Donor
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult ViewCart(int id) {
+            var cart = _db.Carts.Where(c => c.DonorId == id)
+                .Include(c => c.Donor)
+                .Include(c => c.Items.Select(ci => ci.Item))
+                .First();
+
+            return View(cart);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> AddItemToCart(int id, AddToCartViewModel addition) {
+            var isInCart = await _db.CartItems.AnyAsync(ci => ci.CartId == id && ci.Item.Id == addition.WishlistItemId);
+
+            if (isInCart) {
+                return Json(new { WasInCart = true });
+            }
+
+            var cart = await _db.Carts.Where(c => c.DonorId == id)
+                .FirstAsync();
+            var item = await _db.WishlistItems.FindAsync(addition.WishlistItemId);
+            var cartItem = new CartItem {
+                Cart = cart,
+                Item = item,
+                Price = addition.ListPrice,
+                Title = addition.Title
+            };
+
+            _db.CartItems.Add(cartItem);
+            await _db.SaveChangesAsync();
+
+            return Json(new { IsInCart = true });
+        }
+    }
+}
---
+++ Capstone_Wishlist_app/Models/DonorViewModels.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Capstone_Wishlist_app.Models {
+    public class AddToCartViewModel {
+        public int WishlistItemId { get; set; }
+        public decimal ListPrice { get; set; }
+        public string Title { get; set; }
+    }
+}
