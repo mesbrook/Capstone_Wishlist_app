866b88cb3bc1e0344038145037658528a54e5628
eoneillksu <eoneillspsu@gmail.com>
2015-11-23 21:14:03 -0500

    ETO-STORY-20: Added donor and cart entities to data model.

---
+++ Capstone_Wishlist_app/Models/Cart.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Capstone_Wishlist_app.Models {
+    public class Cart {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int DonorId { get; set; }
+
+        [ForeignKey("DonorId")]
+        public virtual Donor Donor { get; set; }
+
+        public virtual ICollection<CartItem> Items { get; set; }
+    }
+
+    public class CartItem {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int CartId { get; set; }
+
+        [Required]
+        public int WishlistItemId { get; set; }
+
+        public string Title { get; set; }
+        public decimal Price { get; set; }
+
+        [ForeignKey("CartId")]
+        public virtual Cart Cart { get; set; }
+
+        [ForeignKey("WishlistItemId")]
+        public virtual WishlistItem Item { get; set; }
+    }
+}
