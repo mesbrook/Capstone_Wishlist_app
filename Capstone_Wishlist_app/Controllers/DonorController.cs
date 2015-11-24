5fa65d684b9345185b66fe7dc70b21bcf575c955
eoneillksu <eoneillspsu@gmail.com>
2015-11-24 16:03:47 -0500

    ETO-STORY-20: Added donor cart view with item removal.

--- Capstone_Wishlist_app/Controllers/DonorController.cs
+++ Capstone_Wishlist_app/Controllers/DonorController.cs
@@ -29,10 +29,21 @@ namespace Capstone_Wishlist_app.Controllers
         public ActionResult ViewCart(int id) {
             var cart = _db.Carts.Where(c => c.DonorId == id)
                 .Include(c => c.Donor)
-                .Include(c => c.Items.Select(ci => ci.Item))
+                .Include(c => c.Items.Select(ci => ci.Item.Wishlist.Child))
                 .First();
+            var items = cart.Items.Select(ci => new CartItemViewModel {
+                CartId = ci.CartId,
+                WishlistItemId = ci.WishlistItemId,
+                ItemId = ci.Item.ItemId,
+                ChildName = ci.Item.Wishlist.Child.FirstName,
+                Price = ci.Price,
+                Title = ci.Title
+            }).ToList();
 
-            return View(cart);
+            return View(new CartViewModel {
+                DonorId = id,
+                Items = items
+            });
         }
 
         [HttpPost]
@@ -57,6 +68,31 @@ namespace Capstone_Wishlist_app.Controllers
             await _db.SaveChangesAsync();
 
             return Json(new { IsInCart = true });
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> RemoveItemFromCart(int id, int wishlistItemId) {
+            var item = await _db.CartItems.FindAsync(id, wishlistItemId);
+            _db.CartItems.Remove(item);
+            await _db.SaveChangesAsync();
+
+            var cart = _db.Carts.Where(c => c.DonorId == id)
+                .Include(c => c.Donor)
+                .Include(c => c.Items.Select(ci => ci.Item.Wishlist.Child))
+                .First();
+            var items = cart.Items.Select(ci => new CartItemViewModel {
+                CartId = ci.CartId,
+                WishlistItemId = ci.WishlistItemId,
+                ItemId = ci.Item.ItemId,
+                ChildName = ci.Item.Wishlist.Child.FirstName,
+                Price = ci.Price,
+                Title = ci.Title
+            }).ToList();
+
+            return PartialView("_CartItems", new CartViewModel {
+                DonorId = id,
+                Items = items
+            });
         }
 
         [HttpGet]
--- Capstone_Wishlist_app/Models/DonorViewModels.cs
+++ Capstone_Wishlist_app/Models/DonorViewModels.cs
@@ -14,4 +14,19 @@ namespace Capstone_Wishlist_app.Models {
         public int DonorId { get; set; }
         public int Count { get; set; }
     }
+
+    public class CartViewModel {
+        public int DonorId { get; set; }
+        public IList<CartItemViewModel> Items { get; set; }
+    }
+
+    public class CartItemViewModel {
+        public int CartId { get; set; }
+        public int WishlistItemId { get; set; }
+        public int WishlistId { get; set; }
+        public string ItemId { get; set; }
+        public string Title { get; set; }
+        public decimal Price { get; set; }
+        public string ChildName { get; set; }
+    }
 }
