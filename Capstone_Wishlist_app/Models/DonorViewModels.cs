693980a7458a1868de4a2e5f3e3b645a29777d78
eoneillksu <eoneillspsu@gmail.com>
2015-11-28 09:44:42 -0500

    ETO-STORY-26: Moved donor registration to the donor controller. Copied registration into a donor view. Registering as a donor now creates an associated donor entity.

--- Capstone_Wishlist_app/Models/DonorViewModels.cs
+++ Capstone_Wishlist_app/Models/DonorViewModels.cs
@@ -5,6 +5,29 @@ using System.Linq;
 using System.Web;
 
 namespace Capstone_Wishlist_app.Models {
+    public class RegisterDonorViewModel {
+        [Display(Name = "Donor's Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+
+        public int? DonorId { get; set; }
+    }
+
     public class AddToCartViewModel {
         public int WishlistItemId { get; set; }
         public decimal ListPrice { get; set; }
